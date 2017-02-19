dc4f024b439955e851317d6535cb403564fde94a
gziny <gil.ziny@gmail.com>
2017-02-19 18:03:35 +0200

    native-base package

--- components/report/report.js
+++ components/report/report.js
@@ -1,19 +1,15 @@
 import React, { PropTypes, Component } from 'react';
-import { Platform, StyleSheet, TouchableHighlight, View, Text, Picker } from 'react-native';
+//import { Platform, StyleSheet, TouchableHighlight, View, Text, Picker } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
+import { Container, Content, Body, ListItem, Text, CheckBox } from 'native-base';
 
 export default class Report extends Component {
   constructor(props) {
     super(props);
     
     //this.goHome = this.goHome.bind(this);
-  }
-  
-  _onPressButton() {
-    console.log("You tapped the report!");
-  } 
 
-  render() {
-    return (
+      /*
         <View>
             <Picker selectedValue="" mode='dropdown' style={styles.picker} >
               <Picker.Item label="" value="" />
@@ -25,7 +21,32 @@ export default class Report extends Component {
                     Tap to return
                 </Text>
             </TouchableHighlight>
-        </View>        
+        </View>   
+        */    
+  }
+  
+  _onPressButton() {
+    console.log("You tapped the report!");
+  } 
+
+  render() {
+    return (
+        <Container>
+          <Content>
+              <ListItem>
+                  <CheckBox checked={true} />
+                  <Body>
+                      <Text>Daily Stand Up</Text>
+                  </Body>
+              </ListItem>
+              <ListItem>
+                  <CheckBox checked={false} />
+                  <Body>
+                      <Text>Discussion with Client</Text>
+                  </Body>
+              </ListItem>
+          </Content>
+      </Container>
     );
   }
 }
